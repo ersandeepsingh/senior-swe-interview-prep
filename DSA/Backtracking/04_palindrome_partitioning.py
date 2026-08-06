964c133c280af32f7887cca0571b69e10a435fc4
ersandeepsingh <cser.sandeepsingh@gmail.com>
2026-08-06 22:30:13 +0530

    solved backtracking

--- DSA/Backtracking/04_palindrome_partitioning.py
+++ DSA/Backtracking/04_palindrome_partitioning.py
@@ -20,9 +20,34 @@
 # - 1 <= s.length <= 16
 # - s contains only lowercase English letters
 
-def partition(s):
-    pass
 
+def partition(s):
+    
+    def palindrome(left,right):
+        while left<right:
+            if s[left] != s[right]:
+                return False
+            left+=1
+            right-=1
+        return True
+
+    result = []
+    path = []
+    
+    def backtrack(start):
+        if start == len(s):
+            result.append(path.copy())
+            return
+        for end in range(start, len(s)):
+            if palindrome(start, end):
+                # choose
+                path.append(s[start:end+1])
+                # explore (note backtrack from end+1, not start+1)
+                backtrack(end+1)
+                # undo
+                path.pop()
+    backtrack(0)
+    return result
 
 if __name__ == '__main__':
     s = "aab"
